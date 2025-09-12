815a2eadb30139ef590b96efb47709be50cb9fc7
Abdelrahman Mohamed <poodymohamed34@gmail.com>
2025-09-12 20:44:14 +0300

    fix: update dependencies and add @headlessui/react

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const port = process.env.PORT || 5000; // project PORT
 
 //--- use cors ---//
 app.use(cors({
-  origin: 'https://vite-frontend-liart.vercel.app',
+  origin: 'https://vite-frontend-liart.vercel.app/',
   credentials: true
 }));
 
