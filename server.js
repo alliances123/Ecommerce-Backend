5b2d9e36c9df7dee726cc39b40b153f2d2b9de9a
Abdelrahman Mohamed <poodymohamed34@gmail.com>
2025-09-12 22:44:00 +0300

    fix: update dependencies

--- server.js
+++ server.js
@@ -23,12 +23,16 @@ app.use(cookieParser());
 const port = process.env.PORT || 5000; // project PORT
 
 //--- use cors ---//
+// app.use(cors({
+//   origin: [
+//     'http://localhost:5173',
+//     'https://vite-frontend-liart.vercel.app',
+//     'https://vite-frontend-git-main-abdelrahman-mohameds-projects-ccc05873.vercel.app',
+//     'https://vite-frontend-cwpwl0gyv-abdelrahman-mohameds-projects-ccc05873.vercel.app'],
+//   credentials: true
+// }));
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://vite-frontend-liart.vercel.app',
-    'https://vite-frontend-git-main-abdelrahman-mohameds-projects-ccc05873.vercel.app',
-    'https://vite-frontend-cwpwl0gyv-abdelrahman-mohameds-projects-ccc05873.vercel.app'],
+  origin: (origin, callback) => callback(null, true),
   credentials: true
 }));
 
